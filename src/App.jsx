0d0c49e9e6f15f01c3d41d26169dd558806b7046
amlanxbghn <amlanxconnect@gmail.com>
2024-08-25 11:54:06 +0530

    Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 
 const App = () => {
@@ -5,6 +6,7 @@ const App = () => {
   return (
     <main className="bg-black">
       <Navbar/>
+      <Hero/>
     </main>
   )
 }
